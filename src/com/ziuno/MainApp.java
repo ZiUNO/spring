68acdc20ddcd2c99626598f7005acc26efc15087
ZiUNO <331245059@qq.com>
2018-08-14 21:02:12 +0800

    System.getProperty("user.dir")

--- src/com/ziuno/MainApp.java
+++ src/com/ziuno/MainApp.java
@@ -1,12 +1,15 @@
 package com.ziuno;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
-        Hello hello = (Hello)applicationContext.getBean("hello");
-        hello.getMessage();
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
+//        Hello hello = (Hello)applicationContext.getBean("hello");
+//        hello.getMessage();
+        ApplicationContext context = new FileSystemXmlApplicationContext(System.getProperty("user.dir") + "/src/Beans.xml");
+        Hello obj = (Hello)context.getBean("hello");
+        obj.getMessage();
     }
 }
